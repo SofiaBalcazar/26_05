40b88d95a3195a116f328d9743552f102c144b62
Balcazar Sofia <sofiabalcazar008@gmail.com>
2026-06-05 08:44:16 -0300

    Formulario

--- Project/Api/views.py
+++ Project/Api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .forms import *
 # Create your views here.
 # ---- resquest retorna la pagina web
 def Home(request):
     return render(request, 'Base.html')
 
 def Registro(request):
-    return render(request, 'Pages/Registro.html')
+    data={ 
+        'Formulario':FormularioRegistro()
+    }
+    return render(request, 'Pages/Registro.html', data)
