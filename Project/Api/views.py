52b46f662fa36e670069a0208f3b2715677e037b
Balcazar Sofia <sofiabalcazar008@gmail.com>
2026-06-09 11:34:41 -0300

    Productos

--- Project/Api/views.py
+++ Project/Api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import *
 # Create your views here.
-# ---- resquest retorna la pagina web
+# ---- request retorna la pagina web
 def Home(request):
     return render(request, 'Base.html')
 
@@ -9,4 +9,18 @@ def Registro(request):
     data={ 
         'Formulario':FormularioRegistro()
     }
+    if request.method=="POST":
+        query=FormularioRegistro(data=request.POST,files=request.FILES)
+        if query.is_valid():
+            query.save()
+            data["Mensaje"]="Datos Registrados"
+        else:
+            data['Mensaje']="No se pudo Registrar"
     return render(request, 'Pages/Registro.html', data)
+
+def VerProductos(request):
+    query= Alumnos.objects.all()
+    data={
+        'Productos':query
+    }
+    return render(request, 'Pages/Productos.html', data)
